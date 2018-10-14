ad2347f56c31a3eee3a8b324de90aae20a488932
Michael Cerasi <michaelcerasi@hotmail.com>
2018-10-14 15:26:01 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,12 @@
+# lib/introduction.rb
+
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
+
+#introduction_with_language
+def introduction(name)
+  puts "Hi, my name is #{name}."
+def introduction_with_language (language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
